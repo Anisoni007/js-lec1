b31e2135ad8c31cc276823b51fbd85f4445f1014
Anisoni007 <156589811+Anisoni007@users.noreply.github.com>
2024-03-13 12:36:56 +0000

    lec1_variables lec3_datatypeconversion

---
+++ 01_basics/lec1_variables.js
@@ -0,0 +1,5 @@
+console.log("Hello javascript");
+let cityName = "jaipur";
+let personName = "Anish soni";
+var personSub = "Btech CSE";
+console.table([cityName,personName,personSub]);
---
+++ 01_basics/lec3_datatypeconversion.js
@@ -0,0 +1,18 @@
+let score = "33";
+// console.log(typeof score);
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2%2);
+// console.log(2**3);
+// console.log(2/2);
+
+// let num1,num2,num3 ;
+// num1 = num2 = num3 = 2
+// console.log(num2);
+// console.log(num3);
+console.log("2" === 2);
+
